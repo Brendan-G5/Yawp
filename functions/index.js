cdf2836ca7151d6790e2f2a95976f571a3636204
Brendan-G5 <brendan.garvey@queensu.ca>
2020-07-07 23:48:04 +0200

    firebase ignore config

--- functions/index.js
+++ functions/index.js
@@ -4,21 +4,9 @@ const app = require('express')();
 
 admin.initializeApp();
 
-const config = {
-  apiKey: "AIzaSyCuwOwBWA4K21lUgTvW2usRswJXMcEYf4M",
-  authDomain: "yawp-a3379.firebaseapp.com",
-  databaseURL: "https://yawp-a3379.firebaseio.com",
-  projectId: "yawp-a3379",
-  storageBucket: "yawp-a3379.appspot.com",
-  messagingSenderId: "552738769198",
-  appId: "1:552738769198:web:1045a7ba1ee08e0af412bf",
-  measurementId: "G-DMHHEMBMWM"
-};
-
+import config from "../firebaseConfig";
 
 const firebase = require('firebase');
-const { DatabaseBuilder } = require('firebase-functions/lib/providers/firestore');
-const { user } = require('firebase-functions/lib/providers/auth');
 firebase.initializeApp(config);
 
 const db = admin.firestore();
