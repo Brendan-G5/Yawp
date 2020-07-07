1d4323a8dd23eaede0ddab94003be46d144b8f21
Brendan-G5 <brendan.garvey@queensu.ca>
2020-07-07 18:47:29 +0200

    some firebase stuff

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  yawp: [
+    {
+      userHandle: 'user',
+      body: 'this is the yawp body',
+      createdAt: '2020-06-12T11:46:01.018Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
