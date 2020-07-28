5cdb0f401b6312b80a5920d8d908e54e032d7e03
Brendan-G5 <brendan.garvey@queensu.ca>
2020-07-28 10:56:51 -0400

    Display Yawps as text

--- yawp-client/src/App.js
+++ yawp-client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 //Components
@@ -36,7 +36,7 @@ function App() {
     <div className="App">
       <Router>
         <NavBar/>
-        <div class="container">
+        <div className="container">
           <Switch>
             <Route exact path="/" component={home}/>
             <Route exact path="/login" component={login}/>
--- yawp-client/src/pages/home.js
+++ yawp-client/src/pages/home.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import axios from 'axios';
 
 export class home extends Component {
+  state = {
+    yawps: null
+  }
+  componentDidMount(){
+    axios.get('/yawps')
+      .then(res => {
+        this.setState({
+          yawps: res.data
+        })
+      })
+      .catch(err => console.error(err))
+  }
   render() {
+    let recentYawpsMarkup = this.state.yawps ? (
+    this.state.yawps.map(yawp => <p>{yawp.body}</p>)
+    ) : <p>Loading...</p>
     return (
-      <div>
-        <h1>home page</h1>
-      </div>
-    )
+    <Grid container spacing={3}>
+      <Grid item sm={8} xs={12}>
+      {recentYawpsMarkup}
+      </Grid>
+      <Grid item sm={4} xs={12}>
+      <p>Profile</p>
+      </Grid>
+    </Grid>
+    );
   }
 }
 
