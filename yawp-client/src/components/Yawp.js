f52624907c07aa39f1fc10371eaa202975242015
Brendan-G5 <brendan.garvey@queensu.ca>
2020-07-28 15:44:22 -0400

    login page init

--- yawp-client/src/components/Yawp.js
+++ yawp-client/src/components/Yawp.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 //MUI
 import Card from '@material-ui/core/Card';
@@ -26,6 +28,7 @@ const styles = {
 
 export class Yawp extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const { classes, yawp : {body, createdAt, userImage, userHandle, yawpId, likeCount, commentCount}} = this.props
     return (
       <Card className = {classes.card}>
@@ -35,7 +38,7 @@ export class Yawp extends Component {
         title = "Profile Image"/>
         <CardContent className={classes.content}>
           <Typography variant='h5' component = {Link} to={`/users/${userHandle}`} color = "primary">{userHandle}</Typography>
-          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
