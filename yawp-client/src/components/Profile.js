9b3c80f11d538bf65a7db1782e75f41b7836c7eb
Brendan-G5 <brendan.garvey@queensu.ca>
2020-07-31 14:35:59 -0400

    Upload image

--- yawp-client/src/components/Profile.js
+++ yawp-client/src/components/Profile.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import theme from '../util/theme';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 
 
@@ -10,7 +10,10 @@ import dayjs from 'dayjs';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import MuiLink from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import Tooltip from '@material-ui/core/Tooltip';
 
 //Icons
 import LocationOn from '@material-ui/icons/LocationOn';
@@ -20,6 +23,7 @@ import CalendarToday from '@material-ui/icons/CalendarToday';
 
 //Redux
 import { connect } from 'react-redux';
+import {logoutUser, uploadImage} from '../redux/actions/userActions';
 
 
 const styles = (theme) => ({
@@ -28,6 +32,18 @@ const styles = (theme) => ({
 
 
 class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+  };
+
+  handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click()
+  }
+
   render() {
     const { classes, user: { credentials: { handle, createdAt, imageUrl, bio, website, location }, loading, authenticated } } = this.props;
 
@@ -36,55 +52,66 @@ class Profile extends Component {
         <div className={classes.profile}>
           <div className="image-wrapper">
             <img src={imageUrl} alt="profile" className='profile-image' />
+            <input
+              type="file"
+              id="imageInput"
+              onChange={this.handleImageChange}
+              hidden="hidden" />
+            <Tooltip title="Edit Profile Picture" placement="right">
+              <IconButton onClick={this.handleEditPicture} className="button">
+                <EditIcon color="primary">
+                </EditIcon>
+              </IconButton>
+            </Tooltip>
           </div>
           <hr />
           <div className="profile-details">
             <MuiLink Component={Link} to={`/users/${handle}`} color="primary" variant="h5">
               @{handle}
             </MuiLink>
-            <hr/>
+            <hr />
             {bio && (
               <Fragment>
                 <Typography variant="body2">{bio}</Typography>
-                <hr/>
+                <hr />
               </Fragment>
             )}
 
             {location && (
               <Fragment>
-                <LocationOn color="primary"/> <span>{location}</span>
-                <hr/>
+                <LocationOn color="primary" /> <span>{location}</span>
+                <hr />
               </Fragment>
             )}
             {website && (
               <Fragment>
-                <LinkIcon color='primary'/>
+                <LinkIcon color='primary' />
                 <a href={website} target="_blank" rel="noopener noreferrer">
                   {'  '}{website}
                 </a>
-                <hr/>
+                <hr />
               </Fragment>
             )}
-            <CalendarToday color='primary'/> {'  '}
+            <CalendarToday color='primary' /> {'  '}
             <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
           </div>
         </div>
       </Paper>
     ) : (
-      <Paper className = {classes.paper}>
-        <Typography variant="body2" align="center">
-          No Profile Found, Please Log in
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No Profile Found, Please Log in
         </Typography>
-        <div className = {classes.buttons}>
-          <Button variant="contained" color="primary" component={Link} to='/login'>
-            Login
+          <div className={classes.buttons}>
+            <Button variant="contained" color="primary" component={Link} to='/login'>
+              Login
           </Button>
-          <Button variant="contained" color="secondary" component={Link} to='/signup'>
-            Signup
+            <Button variant="contained" color="secondary" component={Link} to='/signup'>
+              Signup
           </Button>
-        </div>
-      </Paper>
-    )) : (<p>Loading...</p>)
+          </div>
+        </Paper>
+      )) : (<p>Loading...</p>)
 
     return profileMarkup
   }
@@ -94,9 +121,13 @@ const mapStateToProps = (state) => ({
   user: state.user
 })
 
+const mapActionsToProps = { logoutUser, uploadImage}
+
 Profile.propTypes = ({
   user: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired
 })
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
